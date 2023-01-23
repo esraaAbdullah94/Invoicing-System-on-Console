1d4b00dbdcefdf76c6cd7826d9b0f2dad56faf9e
esraaAbdullah94 <esraa.alsaadi1994@gmail.com>
2023-01-22 22:42:33 -0800

    invoice getters&setters

--- src/shopPackege/Invoice.java
+++ src/shopPackege/Invoice.java
@@ -23,5 +23,63 @@ public class Invoice {
 		this.paidAmount = paidAmount;
 		this.balance = totalAmount - paidAmount; // difference between the total amount and paid amount
 	}
+	//getters and setters methods for each properties.
+
+	public String getCustomerFullName() {
+		return customerFullName;
+	}
+
+	public void setCustomerFullName(String customerFullName) {
+		this.customerFullName = customerFullName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Date getInvoiceDate() {
+		return invoiceDate;
+	}
+
+	public void setInvoiceDate(Date invoiceDate) {
+		this.invoiceDate = invoiceDate;
+	}
+
+	public int getNumberOfItems() {
+		return numberOfItems;
+	}
+
+	public void setNumberOfItems(int numberOfItems) {
+		this.numberOfItems = numberOfItems;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public double getPaidAmount() {
+		return paidAmount;
+	}
+
+	public void setPaidAmount(double paidAmount) {
+		this.paidAmount = paidAmount;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
 
 }
