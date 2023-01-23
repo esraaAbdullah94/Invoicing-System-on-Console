25d554d508ac7f5c9dcb48f0ae14a01e4873432d
esraaAbdullah94 <esraa.alsaadi1994@gmail.com>
2023-01-22 22:32:51 -0800

    Invoice privatr&constructor for properties

--- src/shopPackege/Invoice.java
+++ src/shopPackege/Invoice.java
@@ -1,10 +1,27 @@
 package shopPackege;
 
+import java.util.Date;
+
 public class Invoice {
+	// private properties
+	private String customerFullName;
+	private String phoneNumber;
+	private Date invoiceDate;
+	private int numberOfItems;
+	private double totalAmount;
+	private double paidAmount;
+	private double balance;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// a constructor that takes in previous properties
+	public Invoice(String customerFullName, String phoneNumber, Date invoiceDate, int numberOfItems, double totalAmount,
+			double paidAmount) {
+		this.customerFullName = customerFullName;
+		this.phoneNumber = phoneNumber;
+		this.invoiceDate = invoiceDate;
+		this.numberOfItems = numberOfItems;
+		this.totalAmount = totalAmount;
+		this.paidAmount = paidAmount;
+		this.balance = totalAmount - paidAmount; // difference between the total amount and paid amount
 	}
 
 }
