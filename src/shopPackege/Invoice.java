e512263e2805c0e2d8778e30dc7c49124dfcbddb
esraaAbdullah94 <esraa.alsaadi1994@gmail.com>
2023-01-22 23:48:32 -0800

    invoice private/constructor for properties

--- src/shopPackege/Invoice.java
+++ src/shopPackege/Invoice.java
@@ -1,85 +1,27 @@
 package shopPackege;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Invoice {
 	// private properties
-	private String customerFullName;
-	private String phoneNumber;
+	private String customerName;
+	private String customerPhone;
 	private Date invoiceDate;
-	private int numberOfItems;
+	private ArrayList<Item> items;
 	private double totalAmount;
 	private double paidAmount;
 	private double balance;
 
 	// a constructor that takes in previous properties
-	public Invoice(String customerFullName, String phoneNumber, Date invoiceDate, int numberOfItems, double totalAmount,
-			double paidAmount) {
-		this.customerFullName = customerFullName;
-		this.phoneNumber = phoneNumber;
-		this.invoiceDate = invoiceDate;
-		this.numberOfItems = numberOfItems;
-		this.totalAmount = totalAmount;
-		this.paidAmount = paidAmount;
-		this.balance = totalAmount - paidAmount; // difference between the total amount and paid amount
+	public Invoice(String customerName, String customerPhone) {
+		this.customerName = customerName;
+		this.customerPhone = customerPhone;
+		this.invoiceDate = new Date();
+		this.items = new ArrayList<Item>();
+		this.totalAmount = 0;
+		this.paidAmount = 0;
+		this.balance = 0;
 	}
-	//getters and setters methods for each properties.
-
-	public String getCustomerFullName() {
-		return customerFullName;
-	}
-
-	public void setCustomerFullName(String customerFullName) {
-		this.customerFullName = customerFullName;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public Date getInvoiceDate() {
-		return invoiceDate;
-	}
-
-	public void setInvoiceDate(Date invoiceDate) {
-		this.invoiceDate = invoiceDate;
-	}
-
-	public int getNumberOfItems() {
-		return numberOfItems;
-	}
-
-	public void setNumberOfItems(int numberOfItems) {
-		this.numberOfItems = numberOfItems;
-	}
-
-	public double getTotalAmount() {
-		return totalAmount;
-	}
-
-	public void setTotalAmount(double totalAmount) {
-		this.totalAmount = totalAmount;
-	}
-
-	public double getPaidAmount() {
-		return paidAmount;
-	}
-
-	public void setPaidAmount(double paidAmount) {
-		this.paidAmount = paidAmount;
-	}
-
-	public double getBalance() {
-		return balance;
-	}
-
-	public void setBalance(double balance) {
-		this.balance = balance;
-	}
-	
 
 }
