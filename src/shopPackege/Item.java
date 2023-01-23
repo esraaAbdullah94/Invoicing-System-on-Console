b5ff1a13a7600831e988bc09b5ac69a56227d2aa
esraaAbdullah94 <esraa.alsaadi1994@gmail.com>
2023-01-22 22:56:37 -0800

    Item private&constructor for properties

--- src/shopPackege/Item.java
+++ src/shopPackege/Item.java
@@ -1,5 +1,19 @@
 package shopPackege;
 
 public class Item {
+	private String itemId;
+    private String itemName;
+    private double unitPrice;
+    private int quantity;
+    private double qtyAmount;
+    
+    // constructor
+    public Item(String itemId, String itemName, double unitPrice, int quantity) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+        this.qtyAmount = unitPrice * quantity; //sets the qty amount/price property as the product of the unit price.
+    }
 
 }
