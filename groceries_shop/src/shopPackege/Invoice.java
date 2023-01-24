3e763fd05caa6c3e3cc7313e88f892d50254e79f
esraaAbdullah94 <118810026+esraaAbdullah94@users.noreply.github.com>
2023-01-24 12:04:17 -0800

    LoadData,shopeNmae,InvoiceHeader

--- groceries_shop/src/shopPackege/Invoice.java
+++ groceries_shop/src/shopPackege/Invoice.java
@@ -1,91 +1,197 @@
 package shopPackege;
 
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import java.io.Serializable;
+
 import java.text.ParseException;
+
 import java.text.SimpleDateFormat;
+
 import java.util.Scanner;
+
 import java.util.Date;
+
+import java.util.List;
+
 import java.util.ArrayList;
 
-public class Invoice {
-    private String customerName;
-    private String customerPhone;
-    private Date invoiceDate;
-    private ArrayList<Item> items;
-    private double totalAmount;
-    private double paidAmount;
-    private double balance;
+public class Invoice implements Serializable {
 
-    public Invoice() {
-        this.items = new ArrayList<>();
-        this.totalAmount = 0;
-        this.paidAmount = 0;
-        this.balance = 0;
-        setCustomerName();
-        setCustomerPhone();
-        setInvoiceDate();
-    }
+	private String customerName;
 
-    public void setCustomerName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the customer name: ");
-        this.customerName = scanner.nextLine();
-    }
+	private String customerPhone;
 
-    public void setCustomerPhone() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the customer phone number: ");
-        this.customerPhone = sc.nextLine();
-    }
+	private Date invoiceDate;
 
-    public void setInvoiceDate() {
-        Scanner sc = new Scanner(System.in);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.print("Enter the invoice date (yyyy-MM-dd): ");
-        String dateInput = sc.nextLine();
-        try {
-            this.invoiceDate = dateFormat.parse(dateInput);
-        } catch (ParseException e) {
-            System.out.println("Invalid date format, please enter the date in yyyy-MM-dd format.");
-            setInvoiceDate();
-        }
-    }
-    public void addItem(Item item) {
-        items.add(item);
-        totalAmount += item.getQtyAmount();
-    }
+	private ArrayList<Item> items;
 
-    public void setPaidAmount(double paidAmount) {
-        this.paidAmount = paidAmount;
-        balance = totalAmount - paidAmount;
-    }
+	private double totalAmount;
 
-    public String getCustomerName() {
-        return customerName;
-    }
+	private double paidAmount;
 
-    public String getCustomerPhone() {
-        return customerPhone;
-    }
+	private double balance;
 
-    public Date getInvoiceDate() {
-        return invoiceDate;
-    }
+//4- Report: Statistics (No Of Items, No of Invoices, Total Sales)
 
-    public ArrayList<Item> getItems() {
-        return items;
-    }
+	public static void generateReport(List<Invoice> invoices, String fileName) {
 
-    public double getTotalAmount() {
-        return totalAmount;
-    }
+		int numberOfInvoices = invoices.size();
 
-    public double getPaidAmount() {
-        return paidAmount;
-    }
+		int numberOfItems = 0;
 
-    public double getBalance() {
-        return balance;
-    }
+		double totalSales = 0;
+
+		for (Invoice invoice : invoices) {
+
+			numberOfItems += invoice.getItems().size();
+
+			totalSales += invoice.getTotalAmount();
+
+		}
+
+		try {
+
+			PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+
+			writer.println("Number of Invoices: " + numberOfInvoices);
+
+			writer.println("Number of Items: " + numberOfItems);
+
+			writer.println("Total Sales: " + totalSales);
+
+			writer.close();
+
+			System.out.println("Report has been saved to " + fileName + " successfully.");
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public Invoice() {
+
+		this.items = new ArrayList<>();
+
+		this.totalAmount = 0;
+
+		this.paidAmount = 0;
+
+		this.balance = 0;
+
+		setCustomerName();
+
+		setCustomerPhone();
+
+		setInvoiceDate();
+
+	}
+
+	public void setCustomerName() {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter the customer name: ");
+
+		this.customerName = scanner.nextLine();
+
+	}
+
+	public void setCustomerPhone() {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the customer phone number: ");
+
+		this.customerPhone = sc.nextLine();
+
+	}
+
+	public void setInvoiceDate() {
+
+		Scanner sc = new Scanner(System.in);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+		System.out.print("Enter the invoice date (yyyy-MM-dd): ");
+
+		String dateInput = sc.nextLine();
+
+		try {
+
+			this.invoiceDate = dateFormat.parse(dateInput);
+
+		} catch (ParseException e) {
+
+			System.out.println("Invalid date format, please enter the date in yyyy-MM-dd format.");
+
+			setInvoiceDate();
+
+		}
+
+	}
+
+	public void addItem(Item item) {
+
+		items.add(item);
+
+		totalAmount += item.getQtyAmount();
+
+	}
+
+	public void setPaidAmount(double paidAmount) {
+
+		this.paidAmount = paidAmount;
+
+		balance = totalAmount - paidAmount;
+
+	}
+
+	public String getCustomerName() {
+
+		return customerName;
+
+	}
+
+	public String getCustomerPhone() {
+
+		return customerPhone;
+
+	}
+
+	public Date getInvoiceDate() {
+
+		return invoiceDate;
+
+	}
+
+	public ArrayList<Item> getItems() {
+
+		return items;
+
+	}
+
+	public double getTotalAmount() {
+
+		return totalAmount;
+
+	}
+
+	public double getPaidAmount() {
+
+		return paidAmount;
+
+	}
+
+	public double getBalance() {
+
+		return balance;
+
+	}
+
 }
-
-
--- groceries_shop/src/shopPackege/Menu.java
+++ groceries_shop/src/shopPackege/Menu.java
@@ -1,51 +1,100 @@
 package shopPackege;
 
 import java.util.HashMap;
+
 import java.util.Scanner;
 
 public class Menu {
+
 	private HashMap<String, Runnable> menuOptions;
+
 	private Scanner scanner;
 
 	public Menu() {
+
 		menuOptions = new HashMap<>();
+
 		scanner = new Scanner(System.in);
+
 	}
 
 	public void addOption(String option, Runnable action) {
+
 		menuOptions.put(option, action);
+
+	}
+	public static void goBackToMainMenu() {
+	    System.out.println("Returning to Application Main Menu...");
+	    
 	}
 
 	public void show() {
+
 		while (true) {
-			System.out.println("Main Menu:");
-			System.out.println("1-Shop Settings");
+
+			System.out.println("Application Main Menu: ");
+
+			System.out.println("**Shop Settings**");
+
 			System.out.println("1. Load Data (Items and invoices)");
+
 			System.out.println("2. Set Shop Name (data should be saved)");
+
 			System.out.println("3. Set Invoice Header (Tel / Fax / Email / Website) (Data should be saved)");
+
 			System.out.println("4. Go Back");
+
+			System.out.println("*******************"
+
+					+ "*****************");
 
 			String option = scanner.nextLine();
 
 			switch (option) {
-			case "1. Load Data (Items and invoices)":
+
+			case "1":
+
 				menuOptions.get("1. Load Data (Items and invoices)").run();
+
 				break;
-			case "2. Set Shop Name (data should be saved)":
+
+			case "2":
+
 				menuOptions.get("2. Set Shop Name (data should be saved)").run();
+
 				break;
-			case "3. Set Invoice Header (Tel / Fax / Email / Website) (Data should be saved)":
+
+			case "3":
+
 				menuOptions.get("3. Set Invoice Header (Tel / Fax / Email / Website) (Data should be saved)").run();
+
 				break;
+
 			case "4":
+				goBackToMainMenu();
+
 				break;
+
 			default:
+
 				System.out.println("Invalid option. Please try again.");
+
 			}
-			if (option.equals("4")) {
+
+			if (option.equals("5")) {
+
 				break;
+
 			}
+
 		}
+
+	}
+
+	public double getBalance() {
+
+		return getBalance();
+
 	}
 
 }
